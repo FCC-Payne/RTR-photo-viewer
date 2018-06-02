3121038e52fa360d3cba5445a7c4dbca1d08c14f
Nicholas Petrik <frokrab@gmail.com>
2018-06-01 19:53:25 -0700

    Created database helper function and changed database name

--- database/dataSeed.js
+++ database/dataSeed.js
@@ -4,9 +4,9 @@ const config = require('../config.js');
 const connection = mysql.createConnection({
   user: 'nick',
   password: config.dbPassword,
-  database: 'rtr'
+  database: 'rtrPhotoViewer'
 });
 
 for (let i = 100; i < 200; i++) {
-  connection.query(`INSERT INTO photos (id, picOne, picTwo, picThree, picFour) VALUES (${i}, '${i}-1', '${i}-2', '${i}-3', '${i}-4')`);
+  connection.query(`INSERT INTO photos (id, picOne, picTwo, picThree, picFour) VALUES (${i}, '${i}-1.jpeg', '${i}-2.jpeg', '${i}-3.jpeg', '${i}-4.jpeg')`);
 }
