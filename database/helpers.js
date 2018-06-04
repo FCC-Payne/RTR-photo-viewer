b3e407255ad5312117885d800ef95ec9197c645a
Nicholas Petrik <frokrab@gmail.com>
2018-06-03 17:10:51 -0700

    Wrote untested getPhotos database helper function

--- database/helpers.js
+++ database/helpers.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql');
 const config = require('../config');
+const AWS = require('aws-sdk');
+
+const s3 = AWS.S3({
+  accessKeyId: config.awsAccessKey,
+  secretAccessKey: config.awsSecretKey,
+  region: 'us-west-1'
+});
 
 const connection = mysql.createConnection({
   user: 'nick',
@@ -12,7 +19,28 @@ exports.getEndpoints = (imageId, callback) => {
     if (err) {
       callback(err, null);
     } else {
-      callback(null, results);
+      let endpoints = [results.picOne, results.picTwo, results.picThree, results.picFour]
+      callback(null, endpoints);
     }
   });
+};
+
+exports.getPhotos = (endpoints, callback) => {
+  let promises = endpoints.map((endpoint) => {
+    if (endpoint !== null) {
+      return new Promise((resolve, reject) => {
+        s3.getObject({
+          Bucket: 'fcc-payne-run-the-rentway',
+          key: endpoint
+        }, (err, object) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(object);
+          }
+        });
+      })
+    }
+  });
+  Prmoise.all(promises).then(photos => callback(null, photos)).catch(err => callback(err, null));
 };
