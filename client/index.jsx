407853b9c14a8dc7d8d20267b6941e5d17711142
Nicholas Petrik <frokrab@gmail.com>
2018-06-04 15:48:14 -0700

    Added App method for fetching photo URLs rom server

--- client/index.jsx
+++ client/index.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Thumbnails from './Thumbnails.jsx';
 import Featured from './Featured.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      photoUrls: [
-        'https://i.imgur.com/ju29oAu.jpg',
-        'http://images.unsplash.com/photo-1519612535780-b5d7d96c36f3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=0f6077f459202b771e15e74a79f2d9a9',
-        'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/37936382/1/?bust=1499132738',
-        'https://i.pinimg.com/originals/01/eb/65/01eb650cbc37e942c5bc3b419ae6bad5.jpg'
-      ],
-      featuredPhotoUrl: 'https://i.imgur.com/ju29oAu.jpg'
+      photoUrls: [],
+      featuredPhotoUrl: '',
     };
+    this.getPhotoUrls = this.getPhotoUrls.bind(this);
+  }
+
+  getPhotoUrls(imageId) {
+    axios.get(`http://localhost:3001/${imageId}`)
+      .then(response => {
+        this.setState({
+          photoUrls: response.data,
+          featuredPhotoUrl: response.data[0],
+        })
+      });
+  }
+
+  componentDidMount() {
+    this.getPhotoUrls(126);
   }
 
   render() {
